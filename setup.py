a729e3df5e79b7e7dcdd245805365340d71c9b83
viditagarwal <vidit@elucidata.io>
2019-07-28 22:31:43 +0530

    Updated the setup.py file Added the demo video in the demo/ folder Added the demo video in the README.md file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name= 'pwHash',
     version = '0.1.0',
     packages = ['pwHash'],
+    description = 'Simple Python Package for finding the hash of a string'
+    long_description = readme()
     entry_points = {
         'console_scripts': [
             'pwHash = pwHash.main:hashThis'
